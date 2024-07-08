0072577ccb472613b427019c034ff4199cc8786e
Manaschai Aonon <manaschai_a@cmu.ac.th>
2024-07-08 11:10:26 +0700

    fix CUDA Visible

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@
 #CUDA_VISIBLE_DEVICES=0 python train.py --cfg configs/cuhk_sysu.yaml --resume --ckpt path/to/SOLIDER/log/lup/swin_tiny/checkpoint_tea.pth OUTPUT_DIR './results/cuhk_sysu/swin_tiny' SOLVER.BASE_LR 0.0003 EVAL_PERIOD 5 MODEL.BONE 'swin_tiny' INPUT.BATCH_SIZE_TRAIN 4 MODEL.SEMANTIC_WEIGHT 0.6
 
 # Swin Tiny for Kaggle
-!CUDA_VISIBLE_DEVICES=0 /opt/conda/envs/newCondaEnvironment/bin/python /kaggle/working/SOLIDER-PersonSearch/train.py --cfg /kaggle/working/SOLIDER-PersonSearch/configs/cuhk_sysu.yaml --resume --ckpt /kaggle/input/swin_tiny_pretrained/pytorch/swin_tiny/1/swin_tiny.pth OUTPUT_DIR './results/cuhk_sysu/swin_tiny' SOLVER.BASE_LR 0.0003 EVAL_PERIOD 5 MODEL.BONE 'swin_tiny' INPUT.BATCH_SIZE_TRAIN 4 MODEL.SEMANTIC_WEIGHT 0.6
+CUDA_VISIBLE_DEVICES=0 /opt/conda/envs/newCondaEnvironment/bin/python /kaggle/working/SOLIDER-PersonSearch/train.py --cfg /kaggle/working/SOLIDER-PersonSearch/configs/cuhk_sysu.yaml --resume --ckpt /kaggle/input/swin_tiny_pretrained/pytorch/swin_tiny/1/swin_tiny.pth OUTPUT_DIR './results/cuhk_sysu/swin_tiny' SOLVER.BASE_LR 0.0003 EVAL_PERIOD 5 MODEL.BONE 'swin_tiny' INPUT.BATCH_SIZE_TRAIN 4 MODEL.SEMANTIC_WEIGHT 0.6
